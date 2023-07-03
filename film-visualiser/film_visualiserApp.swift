9e7274e0690a0c37acdc7951b901fbfbb5d38273
Giorgio Mazzei <40761205+giorgiomazzei79@users.noreply.github.com>
2023-07-03 17:05:37 +0200

    Initial Commit

---
+++ film-visualiser/film_visualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  film_visualiserApp.swift
+//  film-visualiser
+//
+//  Created by Giorgio Mazzei on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct film_visualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ film-visualiserUITests/film_visualiserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  film_visualiserUITestsLaunchTests.swift
+//  film-visualiserUITests
+//
+//  Created by Giorgio Mazzei on 03/07/23.
+//
+
+import XCTest
+
+final class film_visualiserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
