781477f05b63de58b2ceec8411b9e00ad2fbf62b
Giorgio Mazzei <40761205+giorgiomazzei79@users.noreply.github.com>
2023-10-28 00:31:00 +0200

    feat: add Movie model

---
+++ film-visualiser/Models/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  film-visualiser
+//
+//  Created by Giorgio Mazzei on 28/10/23.
+//
+
+import Foundation
+
+struct MovieResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let id: Int
+    let title: String
+    let backdropPath: String?
+    let posterPath: String?
+    let overview: String
+    let voteAverage: Double
+    let voteCount: Int
+    let runtime: Int?
+}
